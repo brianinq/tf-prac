a7210b4a6f7045e1b1de3dcc225b9ae3a825e0a0
brian <kipkoechbrian0@gmail.com>
2024-10-04 11:01:16 +0300

    fmt

--- main.tf
+++ main.tf
@@ -1,21 +1,21 @@
-resource "aws_instance" "app_server"{
-	ami = "ami-830c94e3"
-	instance_type = "t2.micro"
-	tags = {
-		Name = "ExampleWebAppServer"
-	}
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "ExampleWebAppServer"
+  }
 }
 
-resource "aws_iam_user" "user"{
-	name = "brian"
-	tags ={
-		Description = "Intern with unnecessay priviledges"
-	}
+resource "aws_iam_user" "user" {
+  name = "brian"
+  tags = {
+    Description = "Intern with unnecessay priviledges"
+  }
 }
 
-resource "aws_iam_policy" "adminAccess"{
-	name = "adminAccess"
-	policy = <<EOF
+resource "aws_iam_policy" "adminAccess" {
+  name   = "adminAccess"
+  policy = <<EOF
 		{
 			"Version" : "2012-10-17",
 			"Statement": [
@@ -29,7 +29,7 @@ resource "aws_iam_policy" "adminAccess"{
 		EOF
 }
 
-resource "aws_iam_user_policy_attachment" "user_admin"{
-	user = aws_iam_user.user.name
-	policy_arn = aws_iam_policy.adminAccess.arn
+resource "aws_iam_user_policy_attachment" "user_admin" {
+  user       = aws_iam_user.user.name
+  policy_arn = aws_iam_policy.adminAccess.arn
 }
